bf5512574fe00affee069b0c5e5ee73c53f8eddc
Vítor Oliveira <vitorsk807@gmail.com>
2023-05-20 21:00:51 -0300

    Finalizando e realizando teste no codigo

--- teste.py
+++ teste.py
@@ -16,12 +16,9 @@ time.sleep(7)
 navegador.get("https://www.instagram.com/p/CsbYeQdLag6/")
 time.sleep(2)
 
+#while 1 == 1:
+navegador.find_element('xpath', '//*[@id="mount_0_0_iw"]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/textarea').click()
+navegador.find_element('xpath', '//*[@id="mount_0_0_iw"]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/textarea').send_keys("G")
+navegador.find_element('xpath', '//*[@id="mount_0_0_iw"]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/div[2]').click()
 
-while True:
-    comentario = ''.join(random.choice(string.ascii_lowercase) for _ in range(5))
-    print(comentario)
-    navegador.find_element('xpath', '//*[@id="mount_0_0_iw"]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/textarea').click()
-    navegador.find_element('xpath', '//*[@id="mount_0_0_iw"]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[3]/div/form/div/textarea').send_keys("G")
-    z += 1
-    print(f"{z}")
 
