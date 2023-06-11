cc381c5390459c805a0ab16ad295d351befbbd73
Vítor Oliveira <vitorsk807@gmail.com>
2023-06-10 23:11:36 -0300

    Retirando usuario e inserindo comentarios

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def abrindo_instagram(username, password, url):
 
 def pegar_seguidores(usuario):
     L = instaloader.Instaloader()
-    L.login('offsvi_', '061219920800601221')
+    L.login('', '')
     profile = instaloader.Profile.from_username(L.context, usuario)
 
     print(f"Salvando Seguidores de {usuario}...")
