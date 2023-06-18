5609048156eda6fc9aa47bafac1b24cd5b6f830e
Vítor Oliveira <vitorsk807@gmail.com>
2023-06-18 20:29:05 -0300

    Atualizando web driver

--- html/teste.py
+++ html/teste.py
@@ -11,7 +11,7 @@ navegador.find_element('xpath', '//*[@id="loginForm"]/div/div[2]/div/label/input
 navegador.find_element('xpath', '//*[@id="loginForm"]/div/div[3]').click()
 time.sleep(7)
 #Indo para o link do sorteio
-navegador.get("https://www.instagram.com/p/CsbYeQdLag6/")
+navegador.get("https://www.instagram.com/p/CsbYeQdLag6/") 
 time.sleep(2)
 
 navegador.find_element('xpath', '//*[@id="mount_0_0_e9"]/div/div/div[3]/div/div/div[1]/div/div[3]/div/div/div/div/div[2]/div/article/div/div[2]/div/div/div[2]/section[1]/span[2]/button/div[2]/svg').click()
